7da292d0e83c116536aab16eeea39a220879da92
s0ffer <soffer322@gmail.com>
2024-08-24 15:46:40 +0500

    Natspec include

--- PracticumTwo.sol
+++ PracticumTwo.sol
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
-
+/// @title My contract from practicum two
+/// @author s0ffer
+/// @notice Here is my imaginary RPG shop mechanics contract
+/// @dev Only for education, practice writing fucntions, data types
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract PracticumTwo is Ownable(msg.sender) {
@@ -38,7 +41,11 @@ contract PracticumTwo is Ownable(msg.sender) {
     // creating array of Payment structures
     Payment[] public paymentsArray;
 
-    // function to set payment data into nested mapping (address > item ID > pushing data to Array) ! ONLY OWNER !
+    /// @notice Set payment data into nested mapping (address > item ID > pushing data to Array)
+    /// @dev Worked in Remix IDE, !ONLY OWNER function!
+    /// @param _user The address to set payment for
+    /// @param _amount The amount of payment
+    /// @param _item The ID of the item that was purchased
     function setPayment(address _user, uint _amount, uint _item) public onlyOwner {
         Payment memory newPayment = Payment({
             amount: _amount,
@@ -48,12 +55,23 @@ contract PracticumTwo is Ownable(msg.sender) {
         
         nestedPayment[_user][_item].push(newPayment); 
     }
-    // function shows data from nested mapping (address > item ID > index in Array > Payment structure)
+
+    
+    /// @notice Shows data from nested mapping (address > item ID > index in Array > Payment structure)
+    /// @dev Worked in Remix IDE
+    /// @param _user The address of user
+    /// @param _item The ID of the item
+    /// @param _paymentIndex The index of the payment
+    /// @return info gets Payment type struct with information about payment
     function getPayment(address _user, uint _item, uint _paymentIndex) public view returns(Payment memory info) {
         require(_paymentIndex <  nestedPayment[_user][_item].length, "Index out of range");
         return nestedPayment[_user][_item][_paymentIndex];
     }
-    // function executes payment, requires 10,000 wei (Item price), pushing payment data into nested mapping (addr > item ID)
+    
+    
+    /// @notice Executes payment, requires 10,000 wei (Item price), pushing payment data into nested mapping (addr > item ID)
+    /// @dev Worked in Remix IDE
+    /// @param _item The ID of the item that you want to buy
     function buyItem(uint _item) public payable {
         require(msg.value == 10000 wei, "Price is 10,000 wei!");
         Payment memory newPayment = Payment({
@@ -68,13 +86,23 @@ contract PracticumTwo is Ownable(msg.sender) {
         newItem.crafted++;  // +1 count for crafted variable in Item structure
     }
 
-    // function reseting payment to zero in nested mapping, ! ONLY OWNER ! 
+    /// @notice Reseting payment to zero in nested mapping
+    /// @dev Worked in Remix IDE, !ONLY OWNER function!
+    /// @param _user The address of the user
+    /// @param _item The ID of the item
+    /// @param _paymentIndex The index of the payment
     function resetPayment(address _user, uint _paymentIndex, uint _item) public onlyOwner {
         require(_paymentIndex <  nestedPayment[_user][_item].length, "Index out of range");
         delete nestedPayment[_user][_item][_paymentIndex];
     }
 
-    // function creating Item structure, includes structure in structure ! ONLY OWNER! 
+    /// @notice Creating [Item] structure, includes structure in structure
+    /// @dev Worked in Remix IDE,  !ONLY OWNER function!
+    /// @param _itemId The index of the new item
+    /// @param _name The name of the new item
+    /// @param _rare The rare of the new item
+    /// @param _damage The damage of the new item
+    /// @param _health The health of the new item
     function setItem(uint _itemId, string memory _name, string memory _rare, uint _damage, uint _health) public onlyOwner {
         Item storage newItem = items[_itemId];
 
@@ -86,7 +114,11 @@ contract PracticumTwo is Ownable(msg.sender) {
         });
     }
 
-    // function shows info about item from items mapping
+    /// @notice Shows info about item from items mapping
+    /// @dev Worked in Remix IDE
+    /// @param _itemId The index of the item
+    /// @param _holderAddr The address of the item holder
+    /// @return ItemReturn type struct information 
     function getItem(uint _itemId, address _holderAddr) public view returns(ItemReturn memory) {
         return ItemReturn({ // created form structure for return value
             name: items[_itemId].name,
@@ -98,12 +130,18 @@ contract PracticumTwo is Ownable(msg.sender) {
         });
     }
 
-    // function that execute another function from this contract for variable to calculate profit 
+    /// @notice Executes another function from this contract for variable to calculate profit
+    /// @dev Worked in Remix IDE
+    /// @param _itemId The index of the item
+    /// @return uint result of profit made by selling items  
     function sumProfit(uint _itemId) public view returns(uint) {
         return getItemAmount(_itemId) * 10000; // variable from another function multiply by 10,000 (WEI)
     }
 
-    // function that gets how many times item crafted, executing for sumProfit function
+    /// @notice Gets how many times item crafted, executing for sumProfit function
+    /// @dev Worked in Remix IDE
+    /// @param _itemId The index of the item
+    /// @return count of how many times item was crafted
     function getItemAmount(uint _itemId) internal view returns(uint count) {
         return items[_itemId].crafted; 
     }
